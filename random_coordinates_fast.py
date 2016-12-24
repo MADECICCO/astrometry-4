8f659cd7febdd2850acb2214a7b0e43c9fdd41f4
N-theory <nathansecrest@msn.com>
2016-12-24 11:59:38 -0500

    Update random_coordinates_fast.py

--- random_coordinates_fast.py
+++ random_coordinates_fast.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python
+"""
+This script will create a chosen number of random coordinates on the celestial sphere.
+There are a few ways of doing this, but the main issue is the pole overdensity that 
+arises from an unaccounted cosine term when creating a uniform grid of RA, Dec.
+
+This script fixes the pole overdensity through weighted random sampling, where the
+weight is proportional to the cosine of the absolute value of the declination.
+That is, a declination of (exactly) +90 or -90 degrees will have zero weight, while
+a declination of 0 has a weight of unity.  
+
+This technique is VERY fast compared with others, such as creating a uniform random
+grid on a cube and choosing points near a radius of 1.
+"""
 
 import numpy as np
 
-N = np.int(1e6) # Number of random coordinates
+N = np.int(1e6) # Number of random coordinates. This is what you choose.
 
 rd = np.column_stack((np.random.uniform(0, 360, N), 
                       np.random.uniform(-90, 90, N)))
